4d8e75b246f5e38d4bd030594df521bb827af8df
algonautshant <shant@algorand.com>
2019-12-02 13:23:12 -0500

    more devel

---
+++ main/src/main/java/freesm/utils/messaging/ReportException.java
@@ -0,0 +1,30 @@
+package freesm.utils.messaging;
+
+public class ReportException {
+
+	public static void errorMessageDefaultAction(String msg) {
+		messageNoThrow(msg, null);
+	}
+
+	public static void errorMessageDefaultAction(String msg, Exception e) {
+		messageNoThrow(msg, e);
+	}
+	
+	public static void messageNoThrow(String msg, Exception e) {
+		if (null != e) {
+			e.printStackTrace();
+		} else {
+			System.err.println(Thread.currentThread().getStackTrace());
+		}
+		System.err.println(msg);
+	}
+	
+	public static void runtimeException(String msg) {
+		throw new RuntimeException(msg);
+	}
+	
+	public static void runtimeException(String msg, Exception e) {
+		e.printStackTrace();
+		throw new RuntimeException(msg);
+	}
+}
